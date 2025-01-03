d3af37c4b8c661f4bff6bfafd541f5bb1197b9f6
Snok <chocowaco152@gmail.noreply.github.com>
2025-01-03 02:28:20 -1000

    Create GameControler object

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function Gameboard() {
         if (board[row][column].getValue() != 0) return;
 
         else board[row][column].addToken(player);
-    }
+    };
 
     printBoard = () => {
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
         console.log(boardWithCellValues);
-    }
+    };
 
     return {getBoard, placeToken, printBoard};
 }
@@ -32,9 +32,62 @@ function Cell() {
 
     addToken = (player) => {
         value = player;
-    }
+    };
 
     getValue = () => value;
 
     return {addToken, getValue};
 }
+
+
+function GameControler(playerOneName = "Player One", playerTwoName = "Player Two") {
+    board = Gameboard();
+
+    const players = [
+        {name: playerOneName, token: 1},
+        {name: playerTwoName, token: 2}
+    ]
+
+    let activePlayer = players[0]
+    
+    switchActivePlayer = () => {
+        if (activePlayer = players[0]) {
+            activePlayer = players[1];
+        }
+        else {
+            activePlayer = players[0];
+        }
+    };
+
+    getActivePlayer = () => activePlayer;
+
+    printNewRound = () => {
+        board.printBoard();
+
+        console.log(`It's ${activePlayer.name}'s turn`);
+        userTokenPlacement();
+    };
+
+    playRound = (row, column) => {
+        console.log(`Placing ${activePlayer.token} into (${row}, ${column})`);
+        board.placeToken(row, column, activePlayer.token);
+
+        switchActivePlayer();
+        printNewRound();
+    };
+    
+    userTokenPlacement = () => {
+        let userRow = prompt(`${activePlayer.name}, what row do you want to place your token?`);
+        let userColumn = prompt(`${activePlayer.name}, what column do you want to place your token?`);
+
+        playRound(userRow, userColumn);
+    };
+
+    //TODO: Add win conditions
+
+    userTokenPlacement();
+
+    return {getActivePlayer};
+}
+
+let game = new GameControler();
